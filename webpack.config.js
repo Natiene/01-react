4a802c4c99147205c0f225c2b5d774b6491227b8
Natiene <cardosonatiene@gmail.com>
2021-09-14 23:02:05 -0300

    update setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -10,6 +13,17 @@ module.exports = {
         extensions: ['.js', '.jsx'],
 
     },
+    devServer: {
+        static: path.resolve(__dirname, 'public')
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ]
+
+    ,
     module: {
         rules: [{
             test: /\.jsx$/,
