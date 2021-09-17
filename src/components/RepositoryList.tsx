73cfb4f9adfd83c6af2ac95efa1f8479a81f4ee8
Natiene <cardosonatiene@gmail.com>
2021-09-17 20:23:00 -0300

    Added: typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -5,8 +5,14 @@ import { useEffect, useState } from 'react';
 
 //https://api.github.com/users/Natiene/repos
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/users/Natiene/repos')
